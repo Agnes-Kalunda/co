d197697e8f1017837327ba46d21933ddcc154bb9
Agnes-Kalunda <agypeter97@gmail.com>
2023-02-06 15:53:37 +0300

    edited components

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,27 +2,32 @@ import React from 'react';
 import Typed from 'react-typed';
 
 const Hero = () => {
-    return (
-        <div className='text-white'>
-            <div className='max-w-[800px ] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
-                <p className='text-[#00df9a] font-bold p-2'>HERO hERO HERO HERO</p>
-                <h1 className='md:text-7xl sn:text-6xl text-4xl font-bold md:py-6'> Grow grow grow.</h1>
-                <div className='flex justify-center items-center '>
-                    <p className='md:text-5xl sn:text-4xl text-xl font-bold'> fast, fast, fast fast</p>
-                    <Typed 
-                    className ="md:text-sxl sm:text-4l text-xl font-bold pl-2"
-                    strings ={['david', 'mike', 'neema']}
-                    typeSpeed={120}
-                    backSpeed= {140}
-                    logo
-                    />
-                </div>
-                <p className='md:text-2xl text-xl font-bold text-grey-500'>hhhhhhh hhhhhhh hhhhhh nnnnnn mmmmm</p>
-
-            </div>
-
+  return (
+    <div className='text-white'>
+      <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
+        <p className='text-[#00df9a] font-bold p-2'>
+          GROWING WITH MBITIS
+        </p>
+        <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold md:py-6'>
+          Grow with data.
+        </h1>
+        <div className='flex justify-center items-center'>
+          <p className='md:text-5xl sm:text-4xl text-xl font-bold py-4'>
+            Grow grow grow with
+          </p>
+          <Typed
+          className='md:text-5xl sm:text-4xl text-xl font-bold md:pl-4 pl-2'
+            strings={['DAVID', 'MIKE', 'NEEMA']}
+            typeSpeed={120}
+            backSpeed={140}
+            loop
+          />
         </div>
-    )
-}
+        <p className='md:text-2xl text-xl font-bold text-gray-500'>Bleh rrrrrr fffff sssssss aaaa DAVID, MIKE, & NEEMAplatforms.</p>
+        <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black'>Get Started</button>
+      </div>
+    </div>
+  );
+};
 
 export default Hero;
