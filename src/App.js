0153c61f7c1b96ed43a7f947e09ae1578722501b
Agnes-Kalunda <agypeter97@gmail.com>
2023-02-06 16:25:18 +0300

    created footer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Section from './components/Section';
 import Cards from './components/cards'
+import Footer from './components/Footer'
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Hero />
       <Section />
       <Cards />
+      <Footer />
      
     </div>
   );
