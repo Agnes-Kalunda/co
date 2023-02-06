2bf825f7b5c7fcec4ff4a56e49afcd0188473ea9
Agnes-Kalunda <agypeter97@gmail.com>
2023-02-06 16:31:04 +0300

    added newsletter component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Hero from './components/Hero';
 import Section from './components/Section';
 import Cards from './components/cards'
 import Footer from './components/Footer'
+import Newsletter from './components/Newsletter';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Navbar />
       <Hero />
       <Section />
+      <Newsletter />
       <Cards />
       <Footer />
      
